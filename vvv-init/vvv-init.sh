0f6816d1c94e8711d480c12bfab38ba1c8f36010
Brad Cavanaugh <bradkcavanaugh@gmail.com>
2014-03-07 13:10:37 -0700

    remove line ending in .sh file.  removing invalid folder.

--- vvv-init/vvv-init.sh
+++ vvv-init/vvv-init.sh
@@ -1,26 +1,19 @@
 #!/bin/bash
 # Init script for a development site with a monolithic Git repo
 # v1.0
-
 # Edit these variables to suit your porpoises
 # -------------------------------------------
-
 # Just a human readable description of this site
 SITE_NAME="Site Name"
 # The name (to be) used by MySQL for the DB
 DB_NAME="site_name"
-
 # ----------------------------------------------------------------
 # You should not need to edit below this point. Famous last words.
-
 echo "Commencing $SITE_NAME setup"
-
 # Reload SSH, to get it to notice the change to known_hosts
 service ssh force-reload
-
 # Make a database, if we don't already have one
 mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS $DB_NAME; GRANT ALL PRIVILEGES ON $DB_NAME.* TO wp@localhost IDENTIFIED BY 'wp';"
-
 DATA_IN_DB=`mysql -u root --password=root --skip-column-names -e "SHOW TABLES FROM $DB_NAME;"`
 if [ "" == "$DATA_IN_DB" ]; then
 	if [ ! -f initial-data.sql ]
@@ -30,8 +23,7 @@ if [ "" == "$DATA_IN_DB" ]; then
 		echo "Loading the database with lovely data"
 		mysql -u root --password=root $DB_NAME < initial-data.sql
 		for TO_SPLIT in ${SEARCH_REPLACE[@]}
-		do
-			SEARCH=`echo $TO_SPLIT |cut -d '|' -f1 `
+		do	SEARCH=`echo $TO_SPLIT |cut -d '|' -f1 `
 			REPLACE=`echo $TO_SPLIT |cut -d '|' -f2 `
 			echo "wp search-replace $SEARCH $REPLACE"
 			wp search-replace $SEARCH $REPLACE
@@ -41,7 +33,5 @@ if [ "" == "$DATA_IN_DB" ]; then
 else
 	echo "Database has data, skipping"
 fi
-
 # The Vagrant site setup script will restart Nginx for us
-
-echo "$SITE_NAME site now installed, you may want to add the user uploaded files";
+echo "$SITE_NAME site now installed, you may want to add the user uploaded files"
